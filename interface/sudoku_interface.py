e3ade337a07a9703777278d7eadc69cb2614b56d
NakaharaNanase <nanametal.0926@gmail.com>
2020-10-18 03:32:29 +0900

    数独の表示をするSdkInterfaceを継承して，入力部のインターフェイスであるSdkEditNumを作成

--- interface/sudoku_interface.py
+++ interface/sudoku_interface.py
@@ -7,11 +7,12 @@ from PyQt5.QtCore import Qt
 
 
 class SdkInterface(QWidget):
-    def __init__(self, window_size, dimention):
+    def __init__(self, window_size, dimention, AnswerArray = None):
         super().__init__()
         self.whsize = window_size
         self.dim = dimention
         self.root_dim = int(math.sqrt(self.dim))
+        self.ans_array = AnswerArray
 
         self.initUI()
     
@@ -23,7 +24,7 @@ class SdkInterface(QWidget):
             for j in range(self.dim):
                 numform = self.nums_form[i][j]
                 # test!
-                numform.setText(f"{i*j}")
+                numform.setText(f"{self.ans_array[i][j]}")
                 
                 numform.setLineWidth(1)
                 numform.setFrameStyle(QFrame.Box | QFrame.Plain)
@@ -49,3 +50,30 @@ class SdkInterface(QWidget):
             x = step * i
             painter.drawLine(x,0,x,rect.height())
             painter.drawLine(0,x,rect.width(),x)
+
+class SdkEditIF(SdkInterface):
+    def initUI(self):
+        self.setFixedSize(self.whsize, self.whsize)
+        self.layout = QGridLayout()
+        self.nums_form = [[QTextEdit() for i in range(self.dim)] for j in range(self.dim)]
+        for i in range(self.dim):
+            for j in range(self.dim):
+                numform = self.nums_form[i][j]
+                # test!
+                numform.setText(f"{i*j}")
+                
+                numform.setLineWidth(1)
+                numform.setFrameStyle(QFrame.Box | QFrame.Plain)
+                numform.setAlignment(Qt.AlignCenter)
+                self.layout.addWidget(numform, i, j)
+        
+        self.layout.setHorizontalSpacing(1)
+        self.layout.setVerticalSpacing(1)
+        self.layout.setContentsMargins(1,1,1,1)
+
+        self.setLayout(self.layout)
+    
+    def clearNums(self):
+        for i in range(self.dim):
+            for j in range(self.dim):
+                self.nums_form[i][j].clear()
