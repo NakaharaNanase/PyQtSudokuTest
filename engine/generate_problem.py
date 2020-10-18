ab092f719161de473e6ede3ff0d4e9058f5ade9d
NakaharaWindows <nanametal.0926@gmail.com>
2020-10-18 20:37:38 +0900

    engineの2ファイルに説明を追加

--- engine/generate_problem.py
+++ engine/generate_problem.py
@@ -1,3 +1,4 @@
+#数字が入力された行列を受け取って，calculation.pyが計算しやすい形にする関数
 def generateProblemArray(QTextEditArray, dimention):
         NumArray = [[0 for i in range(dimention)] for j in range(dimention)]
         #行列の生成
@@ -9,14 +10,3 @@ def generateProblemArray(QTextEditArray, dimention):
                 else:
                     NumArray[i][j] = int(num)
         return (NumArray, dimention)
-
-if __name__ == "__main__":
-    import sys
-    from PyQt5.QtWidgets import QApplication, QTextEdit
-    app = QApplication(sys.argv)
-    dim = 9
-    Array = [[QTextEdit() for i in range(dim)] for j in range(dim)]
-    
-    B = generateProblemArray(Array, dim)
-    print(B)
-    sys.exit(app.exec_())
